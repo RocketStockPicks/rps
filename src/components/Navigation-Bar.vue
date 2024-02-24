a303c4eadf7a5ec6b503bcd4de2d8d61caf7425c
Tanuj Upadhyaya <rocketstockpicks24@gmail.com>
2024-02-24 13:58:04 +0530

    deploy2

---
+++ src/components/Navigation-Bar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app-bar app color="white" flat>
+        <v-badge- color="#D5F0DDB" dot>
+            <v-img src="Logo.jpeg" contain width=50></v-img>
+        </v-badge->
+        <v-toolbar-title class="ml-4">
+            <span class="blue--text">Rocket Stock Picks</span>
+        </v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+
+</script>
+
+<style></style> 
---
+++ src/components/Testinomials.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-row>
+        <v-col>
+            <v-card class="mx-auto" max-width="344">
+                <v-img id="top" src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+                <v-card-title>Hitesh Pant</v-card-title>
+                <v-card-text>
+                    <div align="left">Value for Money</div>
+                </v-card-text>
+
+            </v-card>
+        </v-col>
+        <v-col>
+            <v-card class="mx-auto" max-width="344">
+                <v-img id="top" src="https://cdn.vuetifyjs.com/images/john.jpg"></v-img>
+                <v-card-title>John Pant</v-card-title>
+                <v-card-text>
+                    <div align="left">Very Good Course</div>
+                </v-card-text>
+
+            </v-card>
+        </v-col>
+        <v-col>
+            <v-card class="mx-auto" max-width="344">
+                <v-img id="top" src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+                <v-card-title>Hitesh Pant</v-card-title>
+                <v-card-text>
+                    <div align="left">Value for Money</div>
+                </v-card-text>
+
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<style scoped>
+#top {
+
+    border: 1px solid black;
+
+}
+
+.flexrow {
+    display: flex;
+    flex-direction: column;
+}
+</style>
